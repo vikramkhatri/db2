ce099986729b7d1508165be12158c61203afe512
Vikram Khatri <vikram@zinox.com>
2020-09-07 18:15:24 -0400

    Update db2pc.sh

--- db2pc.sh
+++ db2pc.sh
@@ -2,12 +2,12 @@
 
 echo "Downloading db2-orchestrator ..."
 if [[ $EUID -eq 0 ]]; then
-   curl -L -s https://github.com/vikramkhatri/db2/releases/download/1.0/db2-orchestrator -o /usr/local/bin/db2-orchestrator
+   curl -L -s https://github.com/vikramkhatri/db2/releases/download/v1.0.0/db2-orchestrator -o /usr/local/bin/db2-orchestrator
    chmod +x /usr/local/bin/db2-orchestrator
    echo "Installed /usr/local/bin/db2-orchestrator"
    ls -l /usr/local/bin/db2-orchestrator
 else
-   curl -L -s https://github.com/vikramkhatri/db2/releases/download/1.0/db2-orchestrator -o ./db2-orchestrator
+   curl -L -s https://github.com/vikramkhatri/db2/releases/download/v1.0.0/db2-orchestrator -o ./db2-orchestrator
    chmod +x ./db2-orchestrator
    echo "Installed $HOME/db2-orchestrator"
 fi
