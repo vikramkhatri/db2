a37babeea5d637b2105ab8ec6fa63a1c5398376f
Vikram Khatri <vikram@zinox.com>
2020-09-07 14:19:41 -0400

    Update db2pc.sh
    Fixed bug

--- db2pc.sh
+++ db2pc.sh
@@ -2,13 +2,14 @@
 
 echo "Downloading db2-orchestrator ..."
 if [[ $EUID -eq 0 ]]; then
-   curl -L -s https://github.com/vikramkhatri/db2/releases/download/1.0/db2-orchestrator -o /usr/bin
-   chmod +x /usr/bin/db2-orchestrator
-   echo "You can run db2-orchestrator now"
+   curl -L -s https://github.com/vikramkhatri/db2/releases/download/1.0/db2-orchestrator -o /usr/local/bin/db2-orchestrator
+   chmod +x /usr/local/bin/db2-orchestrator
+   echo "Installed /usr/local/bin/db2-orchestrator"
+   ls -l /usr/local/bin/db2-orchestrator
 else
-   curl -L -s https://github.com/vikramkhatri/db2/releases/download/1.0/db2-orchestrator -o .
+   curl -L -s https://github.com/vikramkhatri/db2/releases/download/1.0/db2-orchestrator -o ./db2-orchestrator
    chmod +x ./db2-orchestrator
-   echo "You can run ./db2-orchestrator now"
+   echo "Installed $HOME/db2-orchestrator"
 fi
 
 echo "Download complete."
